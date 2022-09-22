a97452bffb3786fd75a7d2f9955d3111b2551efe
ThomasCardin <tomcardin@outlook.com>
2022-09-21 21:35:12 -0400

    changed docker ENV var value and names

--- helloworld/main.go
+++ helloworld/main.go
@@ -8,13 +8,30 @@ import (
 
 func main() {
 	r := gin.Default()
-	osValue := os.Getenv("var")
+	helloWorldK8S, err := os.LookupEnv("K8S_HELLO_WORLD")
+	if !err {
+		helloWorldK8S = "k8s env var not found"
+	}
 
-	r.GET("/ping", func(c *gin.Context) {
+	helloWorldDocker, err := os.LookupEnv("DOCKER_HELLO_WORLD")
+	if !err {
+		helloWorldDocker = "docker env not found"
+	}
+
+	// For readinessProbe
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message":    "pong",
-			"os_message": osValue,
+			"message": "200 ok",
 		})
 	})
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message":        "pong",
+			"docker_message": helloWorldK8S,
+			"k8s_message":    helloWorldDocker,
+		})
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
